a1ec618178f7ad7b47756eaf7f43b0fda187b340
ErynErin <fatimaeryn@gmail.com>
2025-07-25 20:27:25 +0800

    Update inventory.gd

--- scenes/inventory_things/inventory.gd
+++ scenes/inventory_things/inventory.gd
@@ -7,11 +7,11 @@ signal update
 @export var slots: Array[InvSlot]
 
 func insert(item: InvItem):
-	var item_slots = slots.filter(func(slots): return slots.item == item)
+	var item_slots = slots.filter(func(slot): return slot.item == item)
 	if !item_slots.is_empty():
 		item_slots[0].amount += 1
 	else:
-		var empty_slots = slots.filter(func(slots): return slots.item == null)
+		var empty_slots = slots.filter(func(slot): return slot.item == null)
 		if !empty_slots.is_empty():
 			empty_slots[0].item = item
 			empty_slots[0].amount = 1
