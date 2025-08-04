acf605ae29c3092f450974a45674878cdb9db88a
Kate Dimatatac <katedimatatac@gmail.com>
2025-08-05 01:38:10 +0800

    sprite sprint
    added sprint :D

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
 const JUMP_VELOCITY = -600.0
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sword: Node2D = $Sword
 
 func _physics_process(delta: float) -> void:
 	var SPEED = GameManager.speed
+	
+# Get the input direction and handle the movement/deceleration.	
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED # go
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED) # stop
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -14,14 +22,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+
+	# Handle sprint.
+	if Input.is_action_pressed("Sprint") and Input.get_axis("left", "right"):
+		velocity.x = move_toward(velocity.x, (velocity.x * 2), SPEED)
+				
 	if direction < 0:
 		animated_sprite.flip_h = true
 	elif direction > 0:
@@ -33,9 +38,11 @@ func _physics_process(delta: float) -> void:
 			sword.sword_attack()
 		if direction == 0:
 			animated_sprite.play("idle")
+		elif Input.is_action_pressed("Sprint"):
+			animated_sprite.play("run")
 		else:
 			animated_sprite.play("walk")
 	else:
-		animated_sprite.play("jump")
+		animated_sprite.play("idle")
 
 	move_and_slide()
