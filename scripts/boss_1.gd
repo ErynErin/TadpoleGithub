e52ae79475c55809cad364a28cfa070a58c939a7
ErynErin <fatimaeryn@gmail.com>
2025-08-21 15:34:23 +0800

    Bosses Dying

--- scripts/boss_1.gd
+++ scripts/boss_1.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+var enemy_health = 50
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func take_damage(amount: int) -> void:
+	enemy_health -= 10
 	animation_player.play("hurt")
-	print("Damage Taken: ", amount)
+	print("Remaining Health: ", enemy_health)
+	if enemy_health <= 0:
+		queue_free()
