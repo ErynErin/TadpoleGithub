4a94476700cf2f7066ebbcb776b4d7f5965fc790
Kate Dimatatac <katedimatatac@gmail.com>
2025-09-01 03:43:24 +0800

    fade in/out for all scenes

---
+++ scenes/Main Scenes/2_nd_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var screen_fade = $CanvasLayer/ScreenFade
+
+func _ready():
+	screen_fade.color.a = 1.0
+	screen_fade.set_z_index(1000)
+	await fade_out_screen()
+	
+func fade_in_screen():
+	var tween = create_tween()
+	tween.tween_property(screen_fade, "color:a", 1.0, 1.5)
+	await tween.finished
+
+func fade_out_screen():
+	var tween = create_tween()
+	tween.tween_property(screen_fade, "color:a", 0.0, 1.5)
+	await tween.finished
